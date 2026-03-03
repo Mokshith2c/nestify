6d821b0fec8b231b12385e9e18da3fe1385cb19d
Mokshith <mokshith643@gmail.com>
2026-03-03 07:52:35 +0530

    Initialized sample data

--- init/index.js
+++ init/index.js
@@ -1,15 +1,19 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 const User = require("../models/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
     .then(() => console.log("connected to DB"))
     .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(`${process.env.ATLASDB_URL}`);
 }
 
 const initDB = async() => {
@@ -32,4 +36,5 @@ const initDB = async() => {
     console.log("data was intialized");
 }
 
-initDB();
+initDB()
+  .then(() => mongoose.connection.close());
--- init/initReviews.js
+++ init/initReviews.js
@@ -1,17 +1,20 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
 const mongoose = require("mongoose");
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
 const User = require("../models/user.js");
 const reviewData = require("./reviewsData.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
     .then(() => console.log("connected to DB"))
     .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(`${process.env.ATLASDB_URL}`);
 }
 
 const initReviews = async() => {
